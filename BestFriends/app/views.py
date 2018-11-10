15e3e5b6ba5a1832e76231173653c2a4a9203aec
Dewei Liu <40216004@ads.qub.ac.uk>
2018-11-10 10:49:42 +0000

    Deleted useless features
    including
    the dump windows in AI app
    prime number in the index page

--- BestFriends/app/views.py
+++ BestFriends/app/views.py
@@ -8,10 +8,8 @@ import requests
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpRequest
-from django.template import RequestContext
 from datetime import datetime
-from BestFriends.settings import prime_number
-from BestFriends.settings import compute_gap
+
 from app.bot_service.ai import AI
 from django.http import JsonResponse
 
@@ -23,7 +21,6 @@ def home(request):
         {
             'title':'Home Page',
             'year':datetime.now().year,
-            'biggest_prime_number':prime_number[0],
 
         })
 
@@ -41,12 +38,6 @@ def js_ai(request):
    
 @csrf_exempt
 def conversation(request):
-    """
-
-    HI Kexin
-    Check out this founction
-    if return a json response instead of an HttpResponse that you should get a dictionary from the response
-    """
     try:
         # Get the body request
         body = (request.body)
@@ -64,39 +55,11 @@ def conversation(request):
 @csrf_exempt
 def ai(request):
     assert isinstance(request, HttpRequest)    
-    try:        
-        question = request.POST['question']
-        print("question is : %s" % question)
-        answer = AI.get_answer(question)
-
-    except:
-        question = 'null'
-        answer = 'null'
-
-
-    # Get a temporary URL for bot service, Following the web page below
-    # https://docs.microsoft.com/en-us/azure/bot-service/bot-service-channel-connect-webchat?view=azure-bot-service-3.0#step-1
-    
-    # URL for the bot service without password filled
-    url_prefix = 'https://webchat.botframework.com/embed/thewaybot?t='
-    permanent_secret = 'aq9Rwy7Yd34.cwA.5tk.EFWM7JrOSTrLsNFU5Ivs2c3-gFz_XvXibfr0ihi2PZU'
-
-    # Get the temporary password
-    r = requests.get(url = "https://webchat.botframework.com/api/tokens", headers = {'Authorization':'BotConnector %s' % (permanent_secret)}) 
-    temporary_token = r.json()
-    
-    # fill the temporary password into the url
-    temporary_secret = url_prefix + temporary_token
-
     return render(request,
         'app/ai.html',
         {
             'title':'AI',
-            #'default_question':'Type your question here',
-            'previous_question':'Your question was: %s' % question,
-            'previous_answer':'Answer from server: %s' % answer,
             'year':datetime.now().year,
-            'temporary_secret':temporary_secret
 
         })    
 
