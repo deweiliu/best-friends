b3e147de0e97dab6980dd7e6a640c3eecd4954d0
deweiliu <deweiliu@hotmail.com>
2018-11-19 14:23:56 +0000

    Updated AI bot password

--- BestFriends/app/variables.py
+++ BestFriends/app/variables.py
@@ -1,4 +1,2 @@
 class V(object):
-    bot_secret='aq9Rwy7Yd34.cwA.5tk.EFWM7JrOSTrLsNFU5Ivs2c3-gFz_XvXibfr0ihi2PZU'
-
-
+    bot_secret='qzV4V-KS1BQ.cwA.6pg.iVbb0ZlWXuFVhhNdkEu29cFhEoClfp0lvAPV1prCcKw'
