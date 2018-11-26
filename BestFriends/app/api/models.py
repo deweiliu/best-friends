6d7ac636a1680255d593d1f49973fac25b0952d2
Dewei Liu <deweiliu@hotmail.com>
2018-11-26 15:09:52 +0000

    Added api for sending a message

--- BestFriends/app/api/models.py
+++ BestFriends/app/api/models.py
@@ -1,1 +1,36 @@
+from app.database.azure_database import AzureDatabase
+from pypika import Query, Table, Field, Order
+def lodge_message(user_id,message,date_time):
+    # Get user details
+    table = Table('USERS')
+    q = Query.from_(table).select('*').where(table.user_id == user_id)
+    result = AzureDatabase.execute(str(q))
+    first_name = result[0][1]
+    surname = result[0][2]
+
+
+    # get the max message id
+    table = Table('MESSAGES')
+    q = Query.from_(table).select('message_index').where(table.user_id == user_id)
+    result = AzureDatabase.execute(str(q))
+    max_index = -1
+    for each in result:
+        index = each[0]
+        if(index > max_index):
+            max_index = index
+    currect_index = max_index + 1
+    print('currect_index= %s' % currect_index)
+
+    # add a record
+    q = Query.into(table).insert(user_id,currect_index,1,first_name,date_time,message)
+
+    result = AzureDatabase.execute(str(q))
+
+
+    response = dict()
+    response['user_id'] = user_id
+    response['datetime'] = date_time
+    response['username'] = {'first name':first_name,'surname':surname}
+    response['message'] = message
+    return response
 
--- BestFriends/app/api/views.py
+++ BestFriends/app/api/views.py
@@ -1,9 +1,9 @@
-import json
+import json, time
 import requests
 from django.http import HttpResponse
 from django.http import HttpRequest
 from datetime import datetime
-from app import models
+from app.api import models
 from app.bot_service.ai import AI
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -20,7 +20,7 @@ def api(request):
         # get request for fetching data from data base
         post = False
     else:
-        raise Exception("The request is neither 'POST' nor 'GET'")
+        return error_response("The request is neither 'POST' nor 'GET'")
 
     # load json data
     # Get the body request
@@ -39,29 +39,53 @@ def post_api(data):
     try:
         type = data['type']
     except:
-        return JsonResponse({'error':'request body must have a key "type"'},status=400)
+        return error_response('request body must have a key "type"',request_method='POST')
 
     if(type == 'send message'):
-        return send_message(data)
+        return send_message('POST',data)
     else:
-        return JsonResponse({'error':'invalid "type" in the POST request'},status=400)
-    return JsonResponse({"answer_dict_name":'hi'})
+        return error_response('invalid "type" in the POST request',request_method='POST')
+
 def get_api(data):
     try:
         type = data['type']
     except:
-        return JsonResponse({'error':'request body must have a key "type"'},status=400)
+        return error_response('request body must have a key "type"',request_method='GET')
 
     if(type == 'history messages'):
-        return history_message(data)
+        return history_message('GET',data)
     elif(type == 'message update'):
-        return message_update(data)
+        return message_update('GET',data)
     else:
         return JsonResponse({'error':'invalid "type" in the GET request'},status=400)
 
-def history_message(data):
+def history_message(request_method,data):
     return JsonResponse({'message':'from history_message'},status=200)
-def send_message(data):
-    return JsonResponse({'message':'from send message'},status=200)
-def message_update(data):
+
+
+def send_message(request_method,data):
+    type = 'send message'
+    try:
+        user_id = data['user_id']
+        message = data['message']
+    except:
+        return error_response('Not enough keys in the request',request_method=request_method,type=type)
+    date_time=time.strftime("%Y-%m-%dT%H:%M:%S")
+    print('datetime = %s'%date_time)
+    response = models.lodge_message(user_id,message,date_time)
+    response['response'] = 'Request received'
+
+    return JsonResponse(response,status=201)
+
+
+def message_update(request_method,data):
     return JsonResponse({'message':'from message_update'},status=200)
+
+
+def error_response(message,request_method='unknown',type='invalid type'):
+    response = dict()
+    response['error'] = 'Bad request'
+    response['request method'] = request_method
+    response['type'] = type
+    response['message'] = message
+    return JsonResponse(response,status=400)
