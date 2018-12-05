4689522d7ab2705343a5fe9aa1d85c3f090f8e51
Dewei Liu <deweiliu@hotmail.com>
2018-12-05 19:55:11 +0000

    fixed bug on login page
    the login page text field value was set as firstname & surname as default
    
    use placehold for prompt instead

--- BestFriends/app/static/app/scripts/login_page.js
+++ BestFriends/app/static/app/scripts/login_page.js
@@ -1,10 +1,18 @@
-var NameSub = document.getElementById("sub_username");
-var NameWords = document.getElementById("first_name");
-NameSub.onclick = function () {
-    if (NameWords.value == "") {
-    alert("please input your name!");
-} else
-    {
-    alert("welcom " + NameWords.value + "!");
-    }
+var surname_text_field = document.getElementById("surname");
+var firstname_text_field = document.getElementById("first_name");
+var submit_username_button = document.getElementById("sub_username");
+surname_text_field.onclick = function () {
+	if (firstname_text_field.value == "") {
+		alert("please enter your first name first!");
+	}
+}
+submit_username_button.onclick = function () {
+	if (firstname_text_field.value == "") {
+		alert("please enter your first name!");
+	} else if (surname_text_field.value == "") {
+		alert("please enter your surname!");
+	}
+	else {
+		alert("welcome " + firstname_text_field.value + "!");
+	}
 }
