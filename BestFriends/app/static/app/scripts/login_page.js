9d3a97db6ba0dd50c85013f30f234f01d94d56de
YaniceWong <18205192203@163.com>
2018-12-08 01:26:49 +0800

    Add the bubble effects to the login page and ai_dialog page.

--- BestFriends/app/static/app/scripts/login_page.js
+++ BestFriends/app/static/app/scripts/login_page.js
@@ -1,18 +1,28 @@
-var surname_text_field = document.getElementById("surname");
-var firstname_text_field = document.getElementById("first_name");
-var submit_username_button = document.getElementById("sub_username");
-surname_text_field.onclick = function () {
-	if (firstname_text_field.value == "") {
-		alert("please enter your first name first!");
-	}
+window.onload = function () {
+    var surname_text_field = document.getElementById("surname");
+    var firstname_text_field = document.getElementById("first_name");
+    var submit_username_button = document.getElementById("sub_username");
+    //$(".bubble").hide();
+    surname_text_field.onclick = function () {
+        if (firstname_text_field.value == "") {
+            alert("please enter your first name first!");
+        }
+    }
+    submit_username_button.onclick = function () {
+        if (firstname_text_field.value == "") {
+            alert("please enter your first name!");
+        } else if (surname_text_field.value == "") {
+            alert("please enter your surname!");
+        }
+        else {
+            document.getElementById("show_bubble").innerHTML = "<span>welcome " + firstname_text_field.value + " ! </span>";
+            //$(".bubble").show();
+        }
+    }
 }
-submit_username_button.onclick = function () {
-	if (firstname_text_field.value == "") {
-		alert("please enter your first name!");
-	} else if (surname_text_field.value == "") {
-		alert("please enter your surname!");
-	}
-	else {
-		alert("welcome " + firstname_text_field.value + "!");
-	}
-}
+
+
+
+    
+    
+
