37b9cfaafa216462cb7a0912c24b2975f045044a
deweiliu <deweiliu@hotmail.com>
2019-01-03 17:58:53 +0000

    bug fixed in the previous commit

--- BestFriends/app/setups.py
+++ BestFriends/app/setups.py
@@ -14,7 +14,7 @@ def database_setup():
     AzureDatabase.create("CONVERSATIONS","conversation_id VARCHAR(40) PRIMARY KEY","user_id INTEGER","token VARCHAR(200)","watermark VARCHAR(8)","FOREIGN KEY(user_id) REFERENCES USERS(user_id)")
     
     # table to store users' conversation history
-    AzureDatabase.create("MESSAGES","user_id INTEGER","message_index INTEGER","is_from_user BIT","sender_name VARCHAR(20)","datetime SMALLDATETIME","message VARCHAR(400)","PRIMARY KEY (user_id, message_index)","FOREIGN KEY (user_id) REFERENCES USERS(user_id)")
+    AzureDatabase.create("MESSAGES","user_id INTEGER","message_index INTEGER","is_from_user BIT","sender_name VARCHAR(20)","datetime DATETIME","message VARCHAR(400)","PRIMARY KEY (user_id, message_index)","FOREIGN KEY (user_id) REFERENCES USERS(user_id)")
 
     # table to store comments from the birthday page
     AzureDatabase.create("BIRTHDAYCOMMENTS","username VARCHAR(40) PRIMARY KEY","comment VARCHAR(200)","datetime VARCHAR(50)")
