dc2417abc31c7661e03d203105832732751d2157
Dewei Liu <deweiliu@hotmail.com>
2020-02-15 00:06:13 +0000

    delete redundant Vistual Studio configuration files

--- BestFriends/app/database/azure_database.py
+++ BestFriends/app/database/azure_database.py
@@ -1,9 +1,17 @@
 import pyodbc
 from app.variables import Database
+
+
 class AzureDatabase(object):
     @staticmethod
     def get_connection():
-        connection = pyodbc.connect(Database.odbc)
+        try:
+            connection = pyodbc.connect(Database.odbc)
+        except:
+            # If the connection is timed out, it is highly likely that your internet service provider (ISP) has blocked your connection. 
+            # Consider changing a network and retry?
+            raise Exception(
+                "Cannot connect to the Azure SQL server. The reason may be the blockage between this machine and the SQL server")
         return connection
 
     @staticmethod
@@ -19,7 +27,7 @@ class AzureDatabase(object):
 
         connection = AzureDatabase.get_connection()
         csr = connection.cursor()
-        print('Executing command %s'%(command))
+        print('Executing command %s' % (command))
         csr.execute(command)
         try:
             rows = csr.fetchall()
@@ -29,18 +37,16 @@ class AzureDatabase(object):
             connection.commit()
             AzureDatabase.close(csr)
         return rows
+
     @staticmethod
-    def create(table_name,*attributes):
+    def create(table_name, *attributes):
         args_str = ", ".join(attributes)
-        command = "IF NOT EXISTS (SELECT * from sysobjects where name='%s') CREATE TABLE %s (%s);" % (table_name,table_name,args_str)
+        command = "IF NOT EXISTS (SELECT * from sysobjects where name='%s') CREATE TABLE %s (%s);" % (
+            table_name, table_name, args_str)
         return AzureDatabase.execute(command)
-
-
 
     @staticmethod
     def drop(table_name):
-        command = "IF EXISTS (SELECT * from sysobjects where name='%s') DROP TABLE dbo.%s;" % (table_name,table_name)
+        command = "IF EXISTS (SELECT * from sysobjects where name='%s') DROP TABLE dbo.%s;" % (
+            table_name, table_name)
         return AzureDatabase.execute(command)
-
-
-        
