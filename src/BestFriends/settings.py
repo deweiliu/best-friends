bfcfb3916e3b4f4de1ad53df7b102535be7df687
Dewei Liu <42348814+deweiliu@users.noreply.github.com>
2022-01-28 18:18:12 +0000

    Update allowed host

--- src/BestFriends/settings.py
+++ src/BestFriends/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 
 # Change the URL to your specific site
-ALLOWED_HOSTS = ['.dliu.com', '.deweiliu.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
