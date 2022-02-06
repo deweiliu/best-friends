ed7d90d8cb58bbf1ca5146eded32c14e79855831
Dewei Liu <42348814+deweiliu@users.noreply.github.com>
2022-02-06 22:04:58 +0000

    Add instance

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -10,7 +10,7 @@ new CdkStack(app, 'BestFriends', {
   domain: 'dliu.com',
   dnsRecord: 'bfa',
   appName: 'best-friends',
-  instanceCount: 1,
+  instanceCount: 2,
   tags: { service: 'best-friends' },
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
